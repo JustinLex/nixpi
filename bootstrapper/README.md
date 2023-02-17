3134b27ab0d8d07b63e3252204ce38cea695974b
Justin Lex-Hammarskjöld <33381901+JustinLex@users.noreply.github.com>
2023-02-17 15:13:19 +0100

    Try to set up porygon

--- bootstrapper/README.md
+++ bootstrapper/README.md
@@ -1,6 +1,6 @@
 These files contain tooling to create a custom NixOS image for raspberry pi, directly from any x86 linux computer.
 
-Only prerequisites are podman installed, root access, and 16G+ memory.
+Only prerequisites are podman installed, root access, and 32G memory.
 
 This method is quite manual, uses a lot of memory, and requires full privileged root permissions to run the qemu vm.
 
@@ -11,17 +11,16 @@ Alternatively, if you have a nixos machine, that can also build this much easier
 
 1. Start with bootstrapper/ as your working directory.
 2. Run `sudo ./build.sh` to build the QEMU vm and open a nix environment inside podman
-3. Run nano to edit /launch-vm.sh to increase disk to 8G, mem to 16G, and (optionally) cores to 16.
+3. Run nano to edit /launch-vm.sh to increase disk to 8G, mem to 24G, and (optionally) cores to 16.
 4. Run /launch-vm.sh to start the nixos VM
-5. Once the VM's shell is open, run the following command:
+5. the vm will create a `nix-vm.XXXX` directory with an empty `xchng` folder in `output/`, outside of the VM. 
+6. Copy in your nix configuration and related files into the `nix-vm.XXXX/xchng/` directory.
+7. Then run the following command:
 ```commandline
-nix-channel --update && nix-env -iA nixos.nixos-generators; img=$(nixos-generate -f sd-aarch64-installer --system aarch64-linux); cp $img /tmp/shared
+nix-channel --update && nix-env -iA nixos.nixos-generators; img=$(nixos-generate -f sd-aarch64-installer --system aarch64-linux -c /tmp/shared/porygon.nix); cp $img /tmp/shared
 ```
 6. You can then run `shutdown now` to stop the VM.
 7. Your newly-built image should now be in `/tmp/nix-vm.XXXXXXXXXX/xchange/shared/`.
 8. It is a good idea to `chmod -R 777 /tmp` here to make the image accessible to non-root outside of the container.
 9. You can now exit the podman container, and you should have your freshly-built image in the output folder.
-
-## NEXT STEPS
-Figure out how to import a nix config into the VM to build the raspi image with?
-Might just copy/paste it in manually
+10. Decompress the image with `zstd -d path to image`, and then dd it onto your sd card.
---
+++ porygon/porygon.nix
@@ -0,0 +1,70 @@
+# Nix config for Porygon, the RasPi3 that handles Zigbee and DNS for my home.
+
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  networking.hostName = "porygon"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.jlh = {
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPLVnXALyHX8qQX7gjzdaRbQqR3YMojIY/aMWuohd661 jlh@charmeleon"
+    ] ++ builtins.split "\n" (builtins.fetchurl https://github.com/JustinLex.keys);
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    htop
+  ];
+
+  hardware.steam-hardware.enable = true;
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = true;
+  };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
+
+  # Configure k3s
+  services.k3s = {
+    enable = true;
+  };
+
+  # Create static pods
+  environment.etc = {
+    "/etc/kubernetes/manifests/pod-hello-world.yaml".source = "/tmp/shared/pod-hello-world.yaml";
+  };
+
+}
+
